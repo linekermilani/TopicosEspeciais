b40eb6e97241a4621ed2382dc6cf03b99f2e7fbe
Lineker Milani <lineker.milani@hotmail.com>
2021-03-26 11:48:10 -0300

    Projeto da Lista de Exercicio

---
+++ ListaExercicios/Exercicio01.js
@@ -0,0 +1,12 @@
+// Ctrl + K + C = Comentar
+// Ctrl + K + U = Tirar o comentário
+// Alt + Shift + F = Identar 
+
+let altura = Math.floor(Math.random() * 100);
+let largura = Math.floor(Math.random() * 100);
+let area = altura * largura;
+
+console.log(`Altura: ${altura} foi gerada randomicamente!`);
+console.log(`Largura: ${largura} foi gerada randomicamente!`);
+console.log("A área do retangulo é: " + area + " m²"); // Concatenação é feita com + (modelo menos usado)
+
---
+++ ListaExercicios/Exercicio02.js
@@ -0,0 +1,1 @@
+console.log("Informe o valor em Real: ");
