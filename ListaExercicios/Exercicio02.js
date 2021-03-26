74be75f52e0bab79c06c25f031609f599c971718
Lineker Milani <lineker.milani@hotmail.com>
2021-03-26 12:31:31 -0300

    Exercicio 02

--- ListaExercicios/Exercicio02.js
+++ ListaExercicios/Exercicio02.js
@@ -1,1 +1,5 @@
-console.log("Informe o valor em Real: ");
+let vreal = Math.floor(Math.random() * 100);
+console.log(`Valor em Real: ${vreal} Reais`);
+console.log(`Valor em Dólar: ${vreal * 5.70} Reais`);
+console.log(`Valor em Euro: ${vreal * 6.73} Reais`);
+console.log(`Valor em Peso Argentino: ${vreal * 0.062} Reais`);
